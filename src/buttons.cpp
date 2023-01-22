1096fac445318e7a654f823df07b7ab5765abb22
Florian Jung <flo@windfis.ch>
2023-01-22 18:22:49 +0100

    button enums

--- src/buttons.cpp
+++ src/buttons.cpp
@@ -12,13 +12,54 @@ static byte midiSetup = 0;
 static bool arpModeHeld;
 static bool assignmentChanged;
 
+enum Button {
+	RECT1 = 2,
+	TRI1 = 12,
+	SAW1 = 4,
+	NOISE1 = 8,
+	RECT2 = 0,
+	TRI2 = 10,
+	SAW2 = 6,
+	NOISE2 = 14,
+	RECT3 = 1,
+	TRI3 = 9,
+	SAW3 = 5,
+	NOISE3 = 13,
+	
+	SYNC1 = 11,
+	RING1 = 3,
+	SYNC2 = 16,
+	RING2 = 20,
+	SYNC3 = 28,
+	RING3 = 24,
+
+	LFO_CHAIN1 = 15,
+	LFO_CHAIN2 = 27,
+	LFO_CHAIN3 = 21,
+
+	LFO_RECT = 30,
+	LFO_SAW = 18,
+	LFO_TRI = 26,
+	LFO_NOISE = 22,
+	LFO_ENV3 = 31,
+
+	ARP_MODE = 32,
+	RETRIG = 29,
+	LOOP = 17,
+
+	PRESET_UP = 19,
+	PRESET_DOWN = 25,
+	PRESET_RESET = 23,
+
+	FILTER_MODE = 7
+};
+
 void buttChanged(byte number, bool value) {
 	if (!value) {
 		// ledNumber(number);
 		//  PRESSED
 		switch (number) {
-
-			case 2:
+			case RECT1:
 				if (!filterModeHeld) {
 					sidShape(0, 1, !bitRead(sid[4], 6));
 					shape1Pressed = true;
@@ -30,8 +71,8 @@ void buttChanged(byte number, bool value) {
 					assignmentChanged = true;
 				}
 				setFilterBit(0);
-				break; // RECT 1
-			case 12:
+				break;
+			case TRI1:
 				if (!filterModeHeld) {
 					sidShape(0, 2, !bitRead(sid[4], 4));
 					shape1Pressed = true;
@@ -43,8 +84,8 @@ void buttChanged(byte number, bool value) {
 					assignmentChanged = true;
 				}
 				setFilterBit(0);
-				break; // TRI 1
-			case 4:
+				break;
+			case SAW1:
 				if (!filterModeHeld) {
 					sidShape(0, 3, !bitRead(sid[4], 5));
 					shape1Pressed = true;
@@ -53,11 +94,11 @@ void buttChanged(byte number, bool value) {
 				} else {
 					filterEnabled[0] = !filterEnabled[0];
 					showFilterAssigns();
-					assignmentChanged = true;
+				assignmentChanged = true;
 				}
 				setFilterBit(0);
-				break; // SAW 1
-			case 8:
+				break;
+			case NOISE1:
 				if (!filterModeHeld) {
 					sidShape(0, 4, !bitRead(sid[4], 7));
 					shape1Pressed = true;
@@ -69,9 +110,9 @@ void buttChanged(byte number, bool value) {
 					assignmentChanged = true;
 				}
 				setFilterBit(0);
-				break; // NOISE 1
+				break;
 
-			case 0:
+			case RECT2:
 				if (!filterModeHeld) {
 					sidShape(1, 1, !bitRead(sid[11], 6));
 					sendMidiButt(41, bitRead(sid[11], 6));
@@ -81,8 +122,8 @@ void buttChanged(byte number, bool value) {
 					assignmentChanged = true;
 				}
 				setFilterBit(1);
-				break; // RECT 2
-			case 10:
+				break;
+			case TRI2:
 				if (!filterModeHeld) {
 					sidShape(1, 2, !bitRead(sid[11], 4));
 					sendMidiButt(42, bitRead(sid[11], 4));
@@ -92,10 +133,10 @@ void buttChanged(byte number, bool value) {
 					assignmentChanged = true;
 				}
 				setFilterBit(1);
-				break; // TRI 2
-			case 6:
+				break;
+			case SAW2:
 				if (!filterModeHeld) {
-					sidShape(1, 3, !bitRead(sid[11], 5));
+				sidShape(1, 3, !bitRead(sid[11], 5));
 					sendMidiButt(43, bitRead(sid[11], 5));
 				} else {
 					filterEnabled[1] = !filterEnabled[1];
@@ -103,8 +144,8 @@ void buttChanged(byte number, bool value) {
 					assignmentChanged = true;
 				}
 				setFilterBit(1);
-				break; // SAW 2
-			case 14:
+				break;
+			case NOISE2:
 				if (!filterModeHeld) {
 					sidShape(1, 4, !bitRead(sid[11], 7));
 					sendMidiButt(44, bitRead(sid[11], 7));
@@ -114,9 +155,9 @@ void buttChanged(byte number, bool value) {
 					assignmentChanged = true;
 				}
 				setFilterBit(1);
-				break; // NOISE 2
+				break;
 
-			case 1:
+			case RECT3:
 				if (!filterModeHeld) {
 					sidShape(2, 1, !bitRead(sid[18], 6));
 					sendMidiButt(45, bitRead(sid[18], 6));
@@ -126,8 +167,8 @@ void buttChanged(byte number, bool value) {
 					assignmentChanged = true;
 				}
 				setFilterBit(2);
-				break; // RECT 3
-			case 9:
+				break;
+			case TRI3:
 				if (!filterModeHeld) {
 					sidShape(2, 2, !bitRead(sid[18], 4));
 					sendMidiButt(46, bitRead(sid[18], 4));
@@ -137,8 +178,8 @@ void buttChanged(byte number, bool value) {
 					assignmentChanged = true;
 				}
 				setFilterBit(2);
-				break; // TRI 3
-			case 5:
+				break;
+			case SAW3:
 				if (!filterModeHeld) {
 					sidShape(2, 3, !bitRead(sid[18], 5));
 					sendMidiButt(47, bitRead(sid[18], 5));
@@ -148,107 +189,107 @@ void buttChanged(byte number, bool value) {
 					assignmentChanged = true;
 				}
 				setFilterBit(2);
-				break; // SAW 3
-			case 13:
+				break;
+			case NOISE3:
 				if (!filterModeHeld) {
 					sidShape(2, 4, !bitRead(sid[18], 7));
-					sendMidiButt(48, bitRead(sid[18], 7));
+				sendMidiButt(48, bitRead(sid[18], 7));
 				} else {
 					filterEnabled[2] = !filterEnabled[2];
 					showFilterAssigns();
 					assignmentChanged = true;
 				}
 				setFilterBit(2);
-				break; // NOISE 3
+				break;
 
-			case 11:
+			case SYNC1:
 				bitWrite(sid[4], 1, !bitRead(sid[4], 1));
 				ledSet(16, bitRead(sid[4], 1));
 				sendMidiButt(49, bitRead(sid[4], 1));
-				break; // SYNC 1
-			case 3:
+				break;
+			case RING1:
 				bitWrite(sid[4], 2, !bitRead(sid[4], 2));
 				ledSet(17, bitRead(sid[4], 2));
 				sendMidiButt(50, bitRead(sid[4], 2));
-				break; // RING 1
+				break;
 
-			case 16:
+			case SYNC2:
 				bitWrite(sid[11], 1, !bitRead(sid[11], 1));
 				ledSet(18, bitRead(sid[11], 1));
 				sendMidiButt(51, bitRead(sid[11], 1));
-				break; // SYNC 2
-			case 20:
+				break;
+			case RING2:
 				bitWrite(sid[11], 2, !bitRead(sid[11], 2));
 				ledSet(19, bitRead(sid[11], 2));
 				sendMidiButt(52, bitRead(sid[11], 2));
-				break; // RING 2
+				break;
 
-			case 28:
+			case SYNC3:
 				bitWrite(sid[18], 1, !bitRead(sid[18], 1));
 				ledSet(20, bitRead(sid[18], 1));
 				sendMidiButt(53, bitRead(sid[18], 1));
-				break; // SYNC 3
-			case 24:
+				break;
+			case RING3:
 				bitWrite(sid[18], 2, !bitRead(sid[18], 2));
 				ledSet(21, bitRead(sid[18], 2));
 				sendMidiButt(54, bitRead(sid[18], 2));
-				break; // RING 3
+				break;
 
-			case 15:
+			case LFO_CHAIN1:
 				lfoButtPressed = true;
 				selectedLfo = 0;
 				if ((lastPot != 9) && (lastPot != 10)) {
 					chain();
 				}
-				break; // LFO CHAIN 1
-			case 27:
+				break;
+			case LFO_CHAIN2:
 				lfoButtPressed = true;
 				selectedLfo = 1;
 				if ((lastPot != 11) && (lastPot != 12)) {
 					chain();
 				}
-				break; // LFO CHAIN 2
-			case 21:
+				break;
+			case LFO_CHAIN3:
 				lfoButtPressed = true;
 				selectedLfo = 2;
 				if ((lastPot != 13) && (lastPot != 14)) {
 					chain();
 				}
-				break; // LFO CHAIN 3
+				break;
 
-			case 30:
+			case LFO_RECT:
 				if (lfoShape[selectedLfo] != 1) {
 					lfoShape[selectedLfo] = 1;
 				} else {
 					lfoShape[selectedLfo] = 0;
 				}
 				showLfo();
-				break; // LFO RECT
-			case 18:
+				break;
+			case LFO_SAW:
 				if (lfoShape[selectedLfo] != 2) {
 					lfoShape[selectedLfo] = 2;
 				} else {
 					lfoShape[selectedLfo] = 0;
 				}
 				showLfo();
-				break; // LFO SAW
-			case 26:
+				break;
+			case LFO_TRI:
 				if (lfoShape[selectedLfo] != 3) {
 					lfoShape[selectedLfo] = 3;
 				} else {
 					lfoShape[selectedLfo] = 0;
 				}
 				showLfo();
-				break; // LFO TRI
-			case 22:
+				break;
+			case LFO_NOISE:
 				if (lfoShape[selectedLfo] != 4) {
 					lfoShape[selectedLfo] = 4;
 				} else {
 					lfoShape[selectedLfo] = 0;
 				}
 				showLfo();
-				break; // LFO NOISE
-			case 31:
+				break;
+			case LFO_ENV3:
 				if (lfoShape[selectedLfo] != 5) {
 					lfoShape[selectedLfo] = 5;
 				} else {
@@ -256,27 +297,27 @@ void buttChanged(byte number, bool value) {
 				}
 				showLfo();
 
-				break; // LFO ENV3
+				break;
 
-			case 32:
+			case ARP_MODE:
 				arpModeHeld = true;
 				if (midiSetup) {
 					midiSetup = 3;
 					digit(99, 0);
 					digit(99, 1);
 				}
-				break; // ARP MODE
+				break;
 
-			case 29:
+			case RETRIG:
 				retrig[selectedLfo] = !retrig[selectedLfo];
 				showLfo();
-				break; //  RETRIG
-			case 17:
+				break;
+			case LOOP:
 				looping[selectedLfo] = !looping[selectedLfo];
 				showLfo();
-				break; //  LOOP
+				break;
 
-			case 19:
+			case PRESET_UP:
 
 				if (midiSetup > 0) {
 					if ((midiSetup == 1) && (masterChannel < 16)) {
@@ -302,16 +343,16 @@ void buttChanged(byte number, bool value) {
 							if (!saveMode) {
 								saveMode = true;
 								saveBounce = 1600;
-							} else {
+				} else {
 								saveBounce = 1600;
 								save();
 							}
 						}
 					}
 				}
-				break; // PRESET UP
+				break;
 
-			case 25:
+			case PRESET_DOWN:
 
 				if (midiSetup > 0) {
 					if ((midiSetup == 1) && (masterChannel > 1)) {
@@ -325,12 +366,10 @@ void buttChanged(byte number, bool value) {
 						ledNumber(masterChannelOut);
 					}
 				} else {
-
 					if (arpModeHeld) {
 						midiSetup = 2;
 						ledNumber(masterChannelOut);
 					} else {
-
 						saveModeTimer = 0;
 						presetDown = true;
 						if (presetUp) {
@@ -343,103 +382,40 @@ void buttChanged(byte number, bool value) {
 							}
 						}
 					}
-					break; // PRESET DOWN
-
-					case 23:
-						resetDown = true;
-						load(1);
-						resetDownTimer = 0;
-						break; // PRESET RESET
-
-					case 7:
-						assignmentChanged = false;
-						filterModeHeld = true;
-						showFilterAssigns();
-
-						break; //  FILTER MODE
 				}
+				break;
+
+			case PRESET_RESET:
+				resetDown = true;
+				load(1);
+				resetDownTimer = 0;
+				break;
+
+			case FILTER_MODE:
+				assignmentChanged = false;
+				filterModeHeld = true;
+				showFilterAssigns();
+
+				break;
 		}
-	}
-
-	else {
-
+	} else {
 		switch (number) {
-
-			case 2:
+			case RECT1:
+			case TRI1:
+			case SAW1:
+			case NOISE1:
 				shape1Pressed = false;
 				shape1PressedTimer = 0;
-				break; // RECT 1
-			case 12:
-				shape1Pressed = false;
-				shape1PressedTimer = 0;
-				break; // TRI 1
-			case 4:
-				shape1Pressed = false;
-				shape1PressedTimer = 0;
-				break; // SAW 1
-			case 8:
-				shape1Pressed = false;
-				shape1PressedTimer = 0;
-				break; // NOISE 1
+				break;
 
-			case 0:
-				break; // RECT 2
-			case 10:
-				break; // TRI 2
-			case 6:
-				break; // SAW 2
-			case 14:
-				break; // NOISE 2
-
-			case 1:
-				break; // RECT 3
-			case 9:
-				break; // TRI 3
-			case 5:
-				break; // SAW 3
-			case 13:
-				break; // NOISE 3
-
-			case 11:
-				break; // SYNC 1
-			case 3:
-				break; // RING 1
-
-			case 16:
-				break; // SYNC 2
-			case 20:
-				break; // RING 2
-
-			case 28:
-				break; // SYNC 3
-			case 24:
-				break; // RING 3
-
-			case 15:
+			case LFO_CHAIN1:
+			case LFO_CHAIN2:
+			case LFO_CHAIN3:
 				lfoButtPressed = false;
 				lfoButtTimer = 0;
-				break; // LFO CHAIN 1
-			case 27:
-				lfoButtPressed = false;
-				lfoButtTimer = 0;
-				break; // LFO CHAIN 2
-			case 21:
-				lfoButtPressed = false;
-				lfoButtTimer = 0;
-				break; // LFO CHAIN 3
-
-			case 30:
-				break; // LFO RECT
-			case 18:
-				break; // LFO SAW
-			case 26:
-				break; // LFO TRI
-			case 22:
-				break; // LFO NOISE
-			case 31:
-				break; // LFO ENV3
-
-			case 32:
+				break;
+			
+			case ARP_MODE:
 				arpModeHeld = false;
 				arpModeCounter = 0;
 				if (!midiSetup) {
@@ -456,15 +432,9 @@ void buttChanged(byte number, bool value) {
 					digit(0, 99);
 					digit(1, 99);
 				}
+				break;
 
-				break; // ARP MODE
-			case 29:
-				break; //  RETRIG
-			case 17:
-				break; //  LOOP
-
-			case 19:
-
+			case PRESET_UP:
 				if (!midiSetup) {
 					presetUp = false;
 					presetScrollSpeed = 10000;
@@ -480,9 +450,8 @@ void buttChanged(byte number, bool value) {
 					saveModeTimer = 0;
 				}
 				scrolled = false;
-				break; // PRESET UP
-			case 25:
-
+				break;
+			case PRESET_DOWN:
 				if (!midiSetup) {
 					presetDown = false;
 					presetScrollSpeed = 10000;
@@ -498,13 +467,13 @@ void buttChanged(byte number, bool value) {
 					saveModeTimer = 0;
 				}
 				scrolled = false;
-				break; // PRESET DOWN
+				break;
 
-			case 23:
+			case PRESET_RESET:
 				resetDown = false;
-				break; // PRESET RESET
+				break;
 
-			case 7:
+			case FILTER_MODE:
 				if (!fatChanged) {
 					unShowFilterAssigns();
 
@@ -520,7 +489,7 @@ void buttChanged(byte number, bool value) {
 					fatChanged = false;
 				}
 				filterModeHeld = false;
-				break; //  FILTER MODE
+				break;
 		}
 	}
 }
