81d081c0f52c41fbbec2f199ab1410b7d6362ff4
Florian Jung <flo@windfis.ch>
2023-02-13 00:35:47 +0100

    cleanup

--- include/ui_vars.h
+++ include/ui_vars.h
@@ -27,29 +27,3 @@ extern int arpModeCounter;
 extern int loadTimer;
 
 extern UiState ui_state;
-
-// FIXME delete
-// preset -> display preset
-// midiSetup = 3 -> digit(99, 0), digit(99,1)
-// midiSetup > 0
-//     masterChannel -> show
-//     masterChannelOut -> show
-//     midiSetup == 1 -> show masterChannel
-//     midiSetup == 2 -> show masterChannelOut
-//     midiSetup == 3 -> digit(00,0), digit(99,1)
-//     midiSetup == 0 -> digit(0,99), 1,99
-
-// lastPot, selectedLfo, filterModeHeld
-
-// arp_mode
-// scale100(voice[i].pulsewidth_base / 2)
-// voice[i].tune_base - 12
-// scaleFine(voice[i].fine_base * 1023.f)
-// voice[i].glide
-// voice[i].attack/decay/sustain/release
-// if (sync) { lfoClockSpeedPending[i] } else { lfo[i].speed / 1.3 }
-// scale100(lfo[i].depth)
-// scale100(cutoff)
-// resonance_base
-// scale100( 1023 - arp_speed_base ) << 2;
-// arp_range_base
